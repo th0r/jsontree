be28fb50909fd3d45b833fe6e1af7dc1268401c6
Konstantin Zhandov <sweetcss@gmail.com>
2014-05-16 12:31:58 +0400

    Added support for options: fold_nodes. Fold all children arrays and objects

--- jsontree.js
+++ jsontree.js
@@ -1,7 +1,7 @@
 !function ($) {
   "use strict"; // jshint ;_;
 
-  var _render = function(obj, p){
+  var _render = function(obj, p, options){
     var parent = $(p);
     var ic = 0;
     var count = 0 ;
@@ -30,13 +30,21 @@
       } else if (typeof obj[key] === 'string'){
         parent.append('<li><span class="key">'+key+':</span><span class="string">"'+obj[key]+'"</span>'+coma+'</li>');
       } else if ($.isArray(obj[key])) {
-        var arval = $('<li><span class="key">'+key+':</span><span class="fold">[</span><ul class="array"></ul><span>]</span>'+coma+'</li>');
+        if (key != 0 && options.fold_nodes) {
+          var arval = $('<li><span class="key">'+key+':</span><span class="fold folded">[</span><ul class="array" style="display: none;"></ul><span>]</span>'+coma+'</li>');
+        } else {
+          var arval = $('<li><span class="key">'+key+':</span><span class="fold">[</span><ul class="array"></ul><span>]</span>'+coma+'</li>');
+        }
         parent.append(arval);
-        arval.find('.unfold').data('card', _render(obj[key], arval.find('.array'))) ;
+        arval.find('.unfold').data('card', _render(obj[key], arval.find('.array'), options)) ;
       }else{
-        var oval = $('<li><span class="key">'+key+':</span><span class="fold">{</span><ul class="object"></ul><span>}</span>'+coma+'</li>');
+        if (key != 0 && options.fold_nodes) {
+          var oval = $('<li><span class="key">'+key+':</span><span class="fold folded">{</span><ul class="object" style="display: none;"></ul><span>}</span>'+coma+'</li>');
+        } else {
+          var oval = $('<li><span class="key">'+key+':</span><span class="fold">{</span><ul class="object"></ul><span>}</span>'+coma+'</li>');
+        }
         parent.append(oval);
-        oval.find('.unfold').data('card', _render(obj[key], oval.find('.object')));
+        oval.find('.unfold').data('card', _render(obj[key], oval.find('.object'), options));
       }
     }
     return ic;
@@ -52,25 +60,29 @@
     $(this).removeClass('folded').parent().find('ul').slideDown();
   });
 
-  var JsonTree = function(self){
+  var JsonTree = function(self, options){
     var j = $.parseJSON(self.data('jsontree'));
     self.append('<ul class="jsontree"></ul>');
-    _render([j], self.find('.jsontree'));
+    _render([j], self.find('.jsontree'), options);
   };
 
-  $.fn.jsontree = function (option) {
+  $.fn.jsontree = function (json, options) {
     return this.each(function () {
       var self = $(this), data = self.data('jsontree');
       if (!data) {
-        if (typeof option == 'string') {
-          data = option;
-          self.data('jsontree', option);
+        if (typeof json == 'string') {
+          data = json;
+          self.data('jsontree', json);
         }else{
           data = {};
           self.data('jsontree', '');
         }
       }
-      new JsonTree(self);
+      var default_options = {
+        fold_nodes: false
+      };
+      options = $.extend(default_options, options || {});
+      new JsonTree(self, options);
     });
   };
 
